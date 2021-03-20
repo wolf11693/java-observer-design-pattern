7d30e13dd434356c9941757a1082cc92f36620ae
Roberto <roberto.amato.11.93@gmail.com>
2021-03-20 19:58:08 +0100

    third commit

--- src/com/ra/pattern/observer/client/MainApp.java
+++ src/com/ra/pattern/observer/client/MainApp.java
@@ -1,32 +1,29 @@
 package com.ra.pattern.observer.client;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.ra.pattern.observer.ItemConsumer;
 import com.ra.pattern.observer.subject.ItemProducer;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		ItemConsumer<String> consumerA = new ItemConsumer<>();
-		ItemConsumer<String> consumerB = new ItemConsumer<>();
-		ItemConsumer<String> consumerC = new ItemConsumer<>();
-		
 		ItemProducer<String> producer = new ItemProducer<>();
-		List<String> list = new ArrayList<>();
-		list.add(new String("Pippo1"));
-		list.add(new String("Pippo2"));
-		list.add(new String("Pippo3"));
-		list.add(new String("Pippo4"));
-		producer.setItemsProduced(list);
-		producer.register(consumerA);
-		producer.register(consumerB);
-		producer.register(consumerC);
+
+		int i=0;
+		for(; i<3; i++) {
+			producer.produceItem(new String("Pippo" + i));
+		}
 		
-		producer.produceItem(new String("Pippo5"));
+		producer.register(new ItemConsumer<String>());
 		
-		ItemConsumer<String> consumerD = new ItemConsumer<>();
-		producer.register(consumerD);
+		for(; i<5; i++) {
+			producer.produceItem(new String("Pippo" + i));
+		}
+		
+		producer.register(new ItemConsumer<String>());
+		producer.register(new ItemConsumer<String>());
+		
+		producer.produceItem(new String("Pippo" + i));
+		
+		producer.register(new ItemConsumer<String>());
 	}
 }
